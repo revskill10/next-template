ce7a959cc62b720ec8e94e2e60607b2602f1f9dd
Dung Truong <checkraiser11@gmail.com>
2018-10-11 20:27:31 +0700

    Add transition

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,44 @@ import App, {Container} from 'next/app'
 import React from 'react'
 import withApolloClient from '../lib/with-apollo-client'
 import { ApolloProvider } from 'react-apollo'
+import { PageTransition } from 'next-page-transitions'
+
 
 class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {}
+ 
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+ 
+    return { pageProps, router }
+  }
+
   render () {
-    const {Component, pageProps, apolloClient} = this.props
+    const {Component, pageProps, apolloClient, router} = this.props
     return <Container>
       <ApolloProvider client={apolloClient}>
-        <Component {...pageProps} />
+        <PageTransition timeout={300} classNames="page-transition">
+          <Component key={router.route} {...pageProps} />
+        </PageTransition>
       </ApolloProvider>
+      <style jsx global>{`
+        .page-transition-enter {
+          opacity: 0;
+        }
+        .page-transition-enter-active {
+          opacity: 1;
+          transition: opacity 300ms;
+        }
+        .page-transition-exit {
+          opacity: 1;
+        }
+        .page-transition-exit-active {
+          opacity: 0;
+          transition: opacity 300ms;
+        }
+      `}</style>
     </Container>
   }
 }
