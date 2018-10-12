09d03a6f6fbe787a042b7d6daa26a58c78a2162b
Dung Truong <checkraiser11@gmail.com>
2018-10-12 15:14:25 +0700

    Add analyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
-module.exports = {
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
+
+const nextConfig = {
+  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/server.html'
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: './bundles/client.html'
+    }
+  },
+  webpack (config) {
+    return config
+  },
   exportPathMap: function () {
     return {
       '/': { page: '/' },
     }
   }
 }
+
+module.exports = withBundleAnalyzer(nextConfig)
