f498b8b2879d2c000f7b3548e4be4f8aa65801a6
Dung Truong <checkraiser11@gmail.com>
2018-10-14 04:19:04 +0700

    Add pwa

--- containers/layouts/app.js
+++ containers/layouts/app.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, {Fragment} from "react";
 import { withStyles } from "@material-ui/core/styles";
 import AppBar from "containers/layouts/app-bar";
 import Drawer from "containers/layouts/drawer";
 import styles from "data/styles";
 import { compose } from 'recompose'
+import Head from 'next/head'
+import Manifest from 'components/manifest'
 
-const App = ({ classes, children }) =>
+const App = ({ classes, children, title, description }) =>
   <div className={classes.root}>
+    <Head>
+      <title>{ title }</title>
+      <meta name="description" content={ description } />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <Manifest />
+    </Head>
     <AppBar />
     <Drawer />
     <main className={classes.content}>
       <div className={classes.toolbar} />
-      {children}
+      <Fragment>
+        {children}
+      </Fragment>
     </main>
   </div>
 
--- pages/index.js
+++ pages/index.js
@@ -5,29 +5,24 @@ import LiveComponent from 'containers/cache-component'
 import {QueryAllLessonClass as query, SubscribeAllLessonClass as subscription} from 'data/graphql/v_all_lesson_class.gql'
 import { compose } from 'recompose'
 import { withI18next } from 'lib/with-i18next'
-import Head from 'next/head'
 
-const Index = ({t, v_all_lesson_class}) =>
-  <React.Fragment>
-  <Head>
-    <title>{t ? t('title.home') : 'Home' }</title>
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-  </Head>
-  <AppLayout>
+const Index = ({t, data}) =>
+  <AppLayout
+    title={t ? t('title.home') : 'Home' }
+    description={t ? t('description.home') : 'HPU Reporting'}
+  >
     <h1>Danh sách buổi học theo tuần: </h1>
     
     <LiveComponent
-      cache={v_all_lesson_class}
+      cache={data}
       subscription={subscription}
     >{DataTable}</LiveComponent>
   </AppLayout>
-  </React.Fragment>
-
 
 Index.getInitialProps = async ({ apolloClient }) => {
-  const tuans = await apolloClient.query({query})
+  const { data } = await apolloClient.query({query})
 
-  return {v_all_lesson_class: tuans.data}
+  return { data }
 }
 
 export default compose(
