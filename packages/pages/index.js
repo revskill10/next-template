6e154094e60bc15fcc186b1caba28e33a5f60066
Dung Truong <checkraiser11@gmail.com>
2018-10-15 19:11:19 +0700

    Fix ssr dnd

--- packages/pages/index.js
+++ packages/pages/index.js
@@ -1,15 +1,12 @@
 import AppLayout from 'containers/layouts/app'
 import DndTest from 'components/dnd-test'
-import NoSSR from 'react-no-ssr'
 
 const IndexPage = () =>
   <AppLayout
     title='Home page'
     description='Simple things'
   >
-  <NoSSR>
     <DndTest />
-  </NoSSR>
     
   </AppLayout>
 
