14847d84b984430f5ee13bb1106e12660471283e
Dung Truong <checkraiser11@gmail.com>
2018-10-19 23:10:48 +0700

    Add date sorting

--- packages/components/datatables/ag-test.js
+++ packages/components/datatables/ag-test.js
@@ -3,6 +3,8 @@ import { AgGridReact } from 'ag-grid-react';
 import { NumberFormatter } from 'components/datatables/number-formatter';
 import { NumericCellEditor} from 'components/datatables/number-cell-editor';
 import { NumericRangeFilter } from 'components/datatables/numeric-range-filter';
+import { dateComparator} from 'components/datatables/date-comparator'
+
 
 class App extends Component {
     constructor(props) {
@@ -10,21 +12,26 @@ class App extends Component {
 
       this.state = {
         columnDefs: [
-            {headerName: 'Make', field: 'make'},
-            {headerName: 'Model', field: 'model'},
-            {
-              headerName: 'Price', 
-              field: 'price', 
-              editable: true, 
-              cellRenderer: 'numberFormatter',
-              cellEditor: 'numericCellEditor',
-              filter: 'numericRangeFilter'
-            }
+          {headerName: 'Make', field: 'make'},
+          {headerName: 'Model', field: 'model'},
+          {
+            headerName: 'Price', 
+            field: 'price', 
+            editable: true, 
+            cellRenderer: 'numberFormatter',
+            cellEditor: 'numericCellEditor',
+            filter: 'numericRangeFilter'
+          },
+          {
+            headerName: 'Date',
+            field: 'date',
+            comparator: dateComparator,
+          }
         ],
         rowData: [
-            {make: 'Toyota', model: 'Celica', price: 35000},
-            {make: 'Ford', model: 'Mondeo', price: 32000},
-            {make: 'Porsche', model: 'Boxter', price: 72000}
+          {make: 'Toyota', model: 'Celica', price: 35000, date: '01/01/2018'},
+          {make: 'Ford', model: 'Mondeo', price: 32000 , date: '01/04/2018'},
+          {make: 'Porsche', model: 'Boxter', price: 72000, date: '01/12/2018'},
         ],
         frameworkComponents: {
           'numberFormatter': NumberFormatter,
@@ -36,20 +43,15 @@ class App extends Component {
 
     render() {
         return (
-            <div
-              className="ag-theme-balham"
-              style={{ height: '200px', width: '600px' }}
-            >
-              <AgGridReact
-                enableFilter={true}
-                pagination={true}
-                enableSorting={true}
-                columnDefs={this.state.columnDefs}
-                rowData={this.state.rowData} 
-                onCellValueChanged={(params) => console.log(params)}  
-                frameworkComponents={this.state.frameworkComponents}
-              />                
-            </div>
+          <AgGridReact
+            enableFilter={true}
+            pagination={true}
+            enableSorting={true}
+            columnDefs={this.state.columnDefs}
+            rowData={this.state.rowData} 
+            onCellValueChanged={(params) => console.log(params)}  
+            frameworkComponents={this.state.frameworkComponents}
+          />                
         );
     }
 }
--- packages/pages/index.js
+++ packages/pages/index.js
@@ -1,6 +1,7 @@
 import AppLayout from 'containers/layouts/app'
 import DndTest from 'components/dnd-test'
 import AgTest from 'components/datatables/ag-test'
+
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
@@ -9,17 +10,13 @@ const IndexPage = () =>
     title='Home page'
     description='Simple things'
   >
-  <div 
-    id="myGrid"
-    style={{
-      boxSizing: "border-box",
-      height: "100%",
-      width: "100%"
-    }}
-    className="ag-theme-balham"    
-  >
-    <AgTest />
-  </div>
+    <div
+      id="grid1"
+      className="ag-theme-balham"
+      style={{ marginTop: '10px', height: '20rem', width: '100%' }}
+    >
+      <AgTest />
+    </div>
     
     <DndTest />
     
