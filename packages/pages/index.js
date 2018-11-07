10a6eea95ed03691b3679a02acb958a9ef9bb66a
Dung Truong <checkraiser11@gmail.com>
2018-11-07 22:58:19 +0700

    Upgrade dx

--- packages/pages/index.js
+++ packages/pages/index.js
@@ -1,10 +1,8 @@
 import AppLayout from 'containers/layout-router'
 import DndTest from 'components/dnd-test'
 import UserTest from 'components/auth/user-info'
-import dynamic from 'next/dynamic'
 
 const IndexPage = () => {
-  const Datatable = dynamic(import('components/charts/datatable'))
   return (
     <AppLayout
       title='Home page'
@@ -12,7 +10,6 @@ const IndexPage = () => {
     >
       <>
       <UserTest />
-      <Datatable />
       <DndTest />
       </>
     </AppLayout>
