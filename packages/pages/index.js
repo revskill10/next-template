87408c1c34dfcaff868558f2c15ed415ed5a928e
Dung Truong <checkraiser11@gmail.com>
2018-11-09 19:55:19 +0700

    Fix crossOrigin

--- packages/pages/index.js
+++ packages/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => {
       description='Simple things'
       meta={<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"
       integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
-      crossorigin=""/>}
+      crossOrigin=""/>}
     >
       <>
       <NoSSR>
